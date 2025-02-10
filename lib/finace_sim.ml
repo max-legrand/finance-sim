6e6accb83bb8f6770c424ed97f3f34dafaffc13e
Maxwell Legrand <max4home@yahoo.com>
2025-02-09 22:18:58 -0500

    Add std dev function

--- lib/finace_sim.ml
+++ lib/finace_sim.ml
@@ -24,8 +24,13 @@ let mean_log_return (log_returns : float list) =
 
 (** Compute the standard deviation of a list of log returns *)
 let get_volatility (log_returns : float list) =
+  let average = mean_log_return log_returns in
+  let sums =
+    List.fold log_returns ~init:0.0 ~f:(fun acc item -> acc +. ((item -. average) ** 2.0))
+  in
+  let sd = sqrt (sums /. Float.of_int (List.length log_returns)) in
+  sd
+;;
 
-
-
-  let compute_next_price current e_return volatility =
-
+let e = 2.718281828459045235360287471352
+let compute_next_price current e_return volatility = ()
